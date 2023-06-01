9ba36e3d349e072efc139d8703bff1092614de2e
Chetan3327 <chauhanchetan12789@gmail.com>
2023-06-02 00:09:49 +0530

    added feedback form

--- index.js
+++ index.js
@@ -5,7 +5,9 @@ const http = require('http');
 const server = http.createServer(app);
 const io = new Server(server);
 const port = 5000;
+require('dotenv').config()
 
+const nodemailer = require('nodemailer')
 app.get('/', (req, res) => {
 	res.sendFile(__dirname + '/index.html');
 });
@@ -18,6 +20,35 @@ io.on('connection', (socket) => {
 	socket.on('send message', (chat) => {
 		io.emit('send message', (chat));
 	});
+	socket.on('send mail', (messageFeedback) => {
+		console.log("0");
+		console.log(messageFeedback);
+		let mailTransporter = nodemailer.createTransport({
+			service:"gmail", 
+			// service:"hotmail",
+			auth: {
+				user:"chauhanchetan12789@gmail.com",
+				pass:process.env.PASS
+			},
+		});
+		
+		let details = {
+			from:"chauhanchetan12789@gmail.com",
+			to:'chauhanchetan12789@gmail.com',
+			subject:"Feedback from chat app",
+			// text:"test content",
+			html: `<p>${messageFeedback}!</p>`
+			// CSS: 'h1{color: red}'
+		};
+		
+		mailTransporter.sendMail(details, (err,info) => {
+			if(err){
+				console.log(err);
+			}else{
+				console.log("mail sent..." + info.response);
+			}
+		})
+	})
 });
 
 server.listen(port, () => {
